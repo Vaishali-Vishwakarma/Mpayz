08e5a2ff56a127591cad26b39665dfd6da3d927c
Vaishali-Vishwakarma <vvishwakarma1710@gmail.com>
2022-08-26 18:07:29 +0530

    Navbar Dashboard added Txn History Added

--- Mpayz/Server/Controllers/WeatherForecastController.cs
+++ Mpayz/Server/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace Mpayz.Server.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 50).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
